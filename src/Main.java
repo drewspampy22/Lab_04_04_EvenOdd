0f0e7fac80813f669b87c99758a97481a6c839fe
drewspampy22 <drewspampy22@gmail.com>
2022-09-15 15:18:45 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int Number = 85;
+        int Test = Number%2;
+        
+        if ( Test == 1)
+        {
+            System.out.println("The Number is Odd");
+        }
+        else
+        {
+            System.out.println("The Number is Even");
+        }
+    }
+}
